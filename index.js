5d67219f6fd93c3cd85d2b372872c66f7d916831
Ricardo <rj.abreu@outlook.com>
2020-03-08 17:49:38 +0000

    product route setup

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('./models/Product');
 
 const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`,
+  process.env.MONGODB_URI ||
+    `mongodb://localhost:27017/mongodb-fullstack-node-app`,
   { useNewUrlParser: true }
 );
 
 app.use(bodyParser.json());
+
+require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
